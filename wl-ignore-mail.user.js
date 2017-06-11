a7c7a9f90c6a81dae33daf5859c06896900e4dbd
Sai Kurogetsu <kurogetsusai@gmail.com>
2017-06-11 11:18:41 +0000

    move localStorage to separate functions

--- wl-ignore-mail.user.js
+++ wl-ignore-mail.user.js
@@ -2,7 +2,7 @@
 // @name        WL Ignore Mail
 // @namespace   wl-ignore-mail
 // @description Adds an option to ignore individual mail threads.
-// @version     0.1.0
+// @version     0.1.1
 // @include     http://www.warlight.net/*
 // @include     https://www.warlight.net/*
 // @grant       none
@@ -15,6 +15,19 @@
  * @returns {void} */
 function turnOffMailRedir() {
 	document.getElementById('MailLink').href = '/Discussion/MyMail';
+}
+
+/** Reads the data stored in local storage.
+ * @returns {object} the data */
+function readData() {
+	return JSON.parse(localStorage['wl-ignore-mail'] || '{"ignored":[]}');
+}
+
+/** Saves the data to local storage.
+ * @param {object} data the data
+ * @returns {void} */
+function saveData(data) {
+	localStorage['wl-ignore-mail'] = JSON.stringify(data);
 }
 
 /** Toggles visibility of the mail icons.
@@ -48,7 +61,7 @@ function setMailIconMode(mode) {
  * @param {number} mailId mail thread's ID
  * @returns {boolean} true if the thread is ignored, false otherwise */
 function isIgnored(mailId) {
-	return (JSON.parse(localStorage['wl-ignore-mail'] || '{"ignored":[]}')).ignored.includes(mailId);
+	return readData().ignored.includes(mailId);
 }
 
 /** Downloads user's My Mail page.
@@ -144,14 +157,14 @@ function fixMyMailPage() {
 			a.style.fontSize = '9px';
 			a.onclick = function (event) {
 				const mailId = parseInt(a.parentNode.querySelector('a').href.split('=')[1], 10);
-				const data = JSON.parse(localStorage['wl-ignore-mail'] || '{"ignored":[]}');
+				const data = readData();
 
 				if (isIgnored(mailId))
 					data.ignored = data.ignored.filter(item => item !== mailId);
 				else
 					data.ignored.push(mailId);
 
-				localStorage['wl-ignore-mail'] = JSON.stringify(data);
+				saveData(data);
 				updateMyMailPageStyles();
 			};
 
